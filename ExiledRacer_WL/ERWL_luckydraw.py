9c8013dec5e1e6b55a63e805e706fa8900dbfd29
hahaschool <hahaschool.wing@gmail.com>
2022-03-23 18:13:09 +0800

    ExiledRacer WL

---
+++ ExiledRacer_WL/ERWL_luckydraw.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import random
+from pprint import pprint
+
+
+# Rinkeby VRF consumer contract address: 0x69a922218413d7c8473e40f5de20c336b759f10f
+# Random number request Tx: https://rinkeby.etherscan.io/tx/0xa479f7d955586767fbb634ab421d608dc078175b4d77bd18b35f0bd8c4c04768#statechange
+# The last random number is: 0xa53a13dae2fd9b3d0aee3f4a0f8031eb58a69f33cff5a0476ab8fcba605d906e
+# We use this as seed
+
+# Random seed is obtained from VRF (see process above)
+random.seed(0xa53a13dae2fd9b3d0aee3f4a0f8031eb58a69f33cff5a0476ab8fcba605d906e)
+
+# Read staking snapshot
+signup_df_reduce = pd.read_csv('ERWL_signup_df_join_reduced.csv')
+signup_df_reduce = signup_df_reduce.T.to_dict()
+
+# Assign raffle tickets to everyone qualified
+total_ticket : int = 0
+reverse_ticket = dict()
+ticket_pool = list()
+
+for cur in signup_df_reduce:
+    cur_amount = signup_df_reduce[cur]['amount_token']
+    cur_addr = signup_df_reduce[cur]['owner']
+    if cur_amount > 999.999:
+        if cur_amount < 4999.999:
+            # 1000~4999: 1 ticket
+            for cur_i in range(1):
+                total_ticket += 1
+                reverse_ticket[total_ticket] = cur_addr
+                ticket_pool.append(total_ticket)
+        elif cur_amount < 9999.999:
+            # 5000~9999: 2 ticket
+            for cur_i in range(2):
+                total_ticket += 1
+                reverse_ticket[total_ticket] = cur_addr
+                ticket_pool.append(total_ticket)
+        elif cur_amount < 49999.999:
+            # 9999~49999: 2 ticket
+            for cur_i in range(3):
+                total_ticket += 1
+                reverse_ticket[total_ticket] = cur_addr
+                ticket_pool.append(total_ticket)
+        else:
+            for cur_i in range(4):
+                total_ticket += 1
+                reverse_ticket[total_ticket] = cur_addr
+                ticket_pool.append(total_ticket)
+
+
+# Output pool
+for cur_ticket in ticket_pool:
+    print(str(reverse_ticket[cur_ticket]) + ' : ' + str(cur_ticket))
+
+# Random shuffle tickets
+random.shuffle(ticket_pool)
+
+
+# Reveal the first 20 tickets
+# The total number of lucky person is 5, in case a person have multiple lucky tockets, the lucky set will be extended
+pprint(ticket_pool[0:20])
+
+# Show addresses and prizes:
+cnt_dict = dict()
+for cur_prize in ticket_pool[0:20]:
+    if cnt_dict.get(reverse_ticket[cur_prize]) is None:
+        cnt_dict[reverse_ticket[cur_prize]] = 0
+    cnt_dict[reverse_ticket[cur_prize]] += 1
+
+pprint(cnt_dict)
---
+++ ExiledRacer_WL/README.md
@@ -0,0 +1,31 @@
+# 255 DAO 2555 GLMR LuckyDraw ExiledRacer Whitelist
+
+## Procedure
+
+1. We took the snapshot of Round 371 for delegator staking amounts.
+2. We joined staking amounts with the sign-up table of this giveaway event, the resulting table (address, amount) is stored here as `ERWL_signup_table_reduced.csv`
+3. We deployed standard Chainlink VRF random generator (using the sample code given in their documentations) on Rinkeby Network at `0x69a922218413d7c8473e40f5de20c336b759f10f`
+4. We requested random numbers (Tx: https://rinkeby.etherscan.io/tx/0xa479f7d955586767fbb634ab421d608dc078175b4d77bd18b35f0bd8c4c04768#statechange), and take the last one as the random seed in our lucky draw: `0xa53a13dae2fd9b3d0aee3f4a0f8031eb58a69f33cff5a0476ab8fcba605d906e`
+5. All registered delegators are sorted by their address lexicographically, delegators without a 1000 GLMR staking are disqualified. We then assigned consecutive integers as their raffle tickets. Assigned tickets are also added to the pool in the meantime.
+6. The whole raffle ticket poll is shuffled (using random seed as specified in Step 4), and the first 100 tickets are taken out as lucky tickets.
+
+To reproduce the lucky draw results, run:
+
+```
+python3 ERWL_luckydraw.py
+```
+
+## 抽奖过程
+
+1. 按规则，我们选取了371轮的质押快照来决定委托人的彩票数量。
+2. 我们整合了抽奖活动注册表格和质押快照，结果存放在 `ERWL_signup_table_reduced.csv` 文件中。
+3. 我们在Rinkeby网络上部署了标准的使用Chainlink VRF生成随机数的合约，合约地址是`0x69a922218413d7c8473e40f5de20c336b759f10f`。
+4. 我们使用刚刚部署的合约从Chainlink预言机请求了随机数（交易地址：https://rinkeby.etherscan.io/tx/0xa479f7d955586767fbb634ab421d608dc078175b4d77bd18b35f0bd8c4c04768#statechange ），然后用得到的最后一个随机数（`0xa53a13dae2fd9b3d0aee3f4a0f8031eb58a69f33cff5a0476ab8fcba605d906e`）当作我们抽奖程序的随机数种子。
+5. 我们对所有注册过抽奖活动的委托人按地址的字典序进行排序，质押数量不足的委托人会被取消资格。然后对于符合资格的委托人我们分配给他们连续的整数作为他们的彩票，同时把彩票放进一个彩票池里。
+6. 我们随机打乱了彩票池，这个随机过程的种子使用的就是我们在步骤4中得到的随机数。
+
+你也可以轻松复现我们的抽奖结果：
+
+```
+python3 ERWL_luckydraw.py
+```
