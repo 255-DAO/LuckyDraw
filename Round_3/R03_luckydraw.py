52f9e215f95619b2f9398e11563abd71f7df8cd6
hahaschool <hahaschool.wing@gmail.com>
2022-05-29 12:26:30 +0800

    Corrected typo in R2 README, added R3 results.

---
+++ Round_3/R03_luckydraw.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import random
+from pprint import pprint
+
+
+# Rinkeby VRF consumer contract address: 0x69a922218413d7c8473e40f5de20c336b759f10f
+# Random number request Tx: https://rinkeby.etherscan.io/tx/0x0f700c9b56146f64c8742d868ed92426c825299a3f9f9179594e17b047bab4ca#statechange
+# The last random number is: 0x5a8b72202bb5706386ef8789a2e3a5152ca960cff30a8676d48cbb5df48ae162
+# We use this as seed
+
+# Random seed is obtained from VRF (see process above)
+random.seed(0x5a8b72202bb5706386ef8789a2e3a5152ca960cff30a8676d48cbb5df48ae162)
+
+# Read staking snapshot
+signup_df_reduce = pd.read_csv('R03_signup_table_reduced.csv')
+signup_df_reduce = signup_df_reduce.T.to_dict()
+
+# Assign raffle tickets to everyone qualified
+total_ticket : int = 0
+reverse_ticket = dict()
+ticket_pool = list()
+for cur in signup_df_reduce:
+    cur_amount = signup_df_reduce[cur]['amount_token']
+    cur_addr = signup_df_reduce[cur]['owner']
+    if cur_amount > 999.999:
+        for cur_i in range(int(cur_amount)):
+            total_ticket += 1
+            reverse_ticket[total_ticket] = cur_addr
+            ticket_pool.append(total_ticket)
+
+# Random shuffle tickets
+random.shuffle(ticket_pool)
+
+
+# Reveal the first 100 tickets as lucky tickets
+pprint(ticket_pool[0:100])
+
+# Show addresses and prizes:
+cnt_dict = dict()
+for cur_prize in ticket_pool[0:100]:
+    if cnt_dict.get(reverse_ticket[cur_prize]) is None:
+        cnt_dict[reverse_ticket[cur_prize]] = 0
+    cnt_dict[reverse_ticket[cur_prize]] += 1
+
+pprint(cnt_dict)
